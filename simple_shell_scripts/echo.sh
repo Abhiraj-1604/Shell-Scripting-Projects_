f7311cd9df8dc69332589cb3310119ababb290ab
Abhiraj Rode <98207222+Abhiraj-1604@users.noreply.github.com>
2024-10-03 17:06:57 +0530

    Create echo.sh

---
+++ simple_shell_scripts/echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+provider="Hostinger"
+echo 'The best hosting provider is $provider'
+echo "The best hosting provider is $provider"
+
+#This simple bash script example will create a new variable and print it out while using different quotation marks.
+
