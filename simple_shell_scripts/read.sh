4a1143d78cb9c5c8e1c6e8c17f79476778612173
Abhiraj Rode <98207222+Abhiraj-1604@users.noreply.github.com>
2024-10-03 17:08:04 +0530

    Create read.sh

---
+++ simple_shell_scripts/read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "What is your age?"
+read age
+echo "Wow, you look younger than $age years old"
+
+#To take input from users, we’ll use the read bash command. First, create a new bash shell file:
